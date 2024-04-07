68da6f70b9b2d2e763ab8259c2a962af45edc675
stevenwinnick <shw2139@columbia.edu>
2024-04-07 14:55:04 -0400

    Added more scaling tests

--- scaling/scaling.h
+++ scaling/scaling.h
@@ -4,6 +4,7 @@
 double phaseDifference(float real1, float imag1, float real2, float imag2);   
 void processTransformed(float* realPrev, float* imagPrev, float* realNew,
                         float* imagNew, float* realOutPrev, float* imagOutPrev,
-                        float* realOutNew, float* imagOutNew);   
+                        float* realOutNew, float* imagOutNew,
+                        double phaseScaleAmount);   
 
 #endif
