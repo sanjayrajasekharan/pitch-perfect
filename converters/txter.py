0cc8c71d4e8c6b654ee716aa2b4c82998e6186d7
sanjay <sanjay.rajasekharan@gmail.com>
2024-04-20 19:59:52 -0400

    support both mono and multichannel wav files

--- converters/txter.py
+++ converters/txter.py
@@ -1,5 +1,6 @@
 import sys
 from scipy.io import wavfile
+import numpy as np
 
 samplerate, data = wavfile.read(sys.argv[1])
 
@@ -8,13 +9,13 @@ if len(sys.argv) > 2:
     with open(sys.argv[2], 'w') as file:
         for sample in data:
             # if sample is a list, write the first element
-            if isinstance(sample, list):
+            if isinstance(sample, np.ndarray):
                 file.write(str(sample[0]) + '\n')
             else:
                 file.write(str(sample) + '\n')
 else:
     for sample in data:
-        if isinstance(sample, list):
+        if isinstance(sample, np.ndarray):
             print(sample[0])
         else:
             print(sample)
